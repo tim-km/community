39bd88763eccbae7207ee457d1688b79ea9823f6
tim-km <vanillafloyd@protonmail.com>
2020-08-19 22:49:45 +0300

    removed list import

--- src/main/java/com/example/community/rest/ProgrammingLanguageRestController.java
+++ src/main/java/com/example/community/rest/ProgrammingLanguageRestController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("api/programminglanguage")
 public class ProgrammingLanguageRestController {
--- src/main/java/com/example/community/service/IProgrammingLanguageService.java
+++ src/main/java/com/example/community/service/IProgrammingLanguageService.java
@@ -2,8 +2,6 @@ package com.example.community.service;
 
 import com.example.community.entity.ProgrammingLanguage;
 
-import java.util.List;
-
 public interface IProgrammingLanguageService {
 
     Iterable<ProgrammingLanguage> getProgrammingLanguages();
